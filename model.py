2160a495a29e70731651604b8227c16be083d5dc
johnvparsons99 <62982818+johnvparsons99@users.noreply.github.com>
2022-02-10 15:52:40 +1000

    Began working on GCNLayer

--- model.py
+++ model.py
@@ -2,6 +2,33 @@ import tensorflow as tf
 from tensorflow import keras
 
 
+class GCNLayer(tf.keras.layers.Layer):
+    """
+    My custom network layer.
+    """
+    def __init__(self, adj_m, test_adj_m):
+        super(GCNLayer, self).__init__()
+        self.adj_m = adj_m
+        self.test_adj_m = test_adj_m
+        self.w = tf.zeros_like(self.adj_m)
+
+    def build(self, input_shape):
+        self.w = self.add_weight("w",
+                                       shape=(1, input_shape[-1]),
+                                       initializer=keras.initializers.initializers_v1.RandomNormal)
+
+    def call(self, feature_matrix, training=None, ):
+        feature_matrix = tf.squeeze(feature_matrix)
+        if training:
+            ax = tf.sparse.sparse_dense_matmul(tf.cast(self.adj_m, float), feature_matrix)
+            z = ax * self.weights1
+        else:
+            ax = tf.sparse.sparse_dense_matmul(tf.cast(self.test_adj_m, float), feature_matrix)
+            z = ax * self.weights1
+
+        return z
+
+
 class Model(tf.keras.Model):
 
     def __init__(self):
